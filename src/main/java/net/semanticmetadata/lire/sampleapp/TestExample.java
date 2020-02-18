d69f2a2b1f7528183586f28328cbf98f091369ad
Cristian Abrante <cristian@abrante.me>
2020-02-18 17:04:36 +0100

    initial structure created

---
+++ src/main/java/net/semanticmetadata/lire/sampleapp/TestExample.java
@@ -0,0 +1,19 @@
+package net.semanticmetadata.lire.sampleapp;
+
+import net.semanticmetadata.lire.imageanalysis.features.GlobalFeature;
+import net.semanticmetadata.lire.imageanalysis.features.global.CEDD;
+import net.semanticmetadata.lire.utils.FileUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TestExample {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
